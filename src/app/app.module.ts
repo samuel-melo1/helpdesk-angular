74ebdc8637b51a4f28112fc090ab3ca47e7bff96
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-09-30 21:51:13 -0300

    header component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,14 +21,16 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
 import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
-import { HomeComponent } from './components/home/home.component'
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
