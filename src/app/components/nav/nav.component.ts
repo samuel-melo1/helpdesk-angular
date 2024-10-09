085e984fadcfda92e4a89f84f2d2b3e393e364e2
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-10-09 20:26:46 -0300

    improve login css

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -13,13 +13,13 @@ export class NavComponent implements OnInit {
   constructor(private router: Router, private authService: AuthService, private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/read/1'])
+    this.router.navigate(['home'])
   }
 
   logout(){
     this.authService.logout(); 
     this.router.navigate(['login']);
-    this.toast.info('Logout realizado com sucesso!', 'Logout', {timeOut: 7000})
+    this.toast.info('Logout realizado com sucesso!', 'Logout')
   }
 
 }
