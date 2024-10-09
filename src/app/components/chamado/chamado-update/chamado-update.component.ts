1fbf4a865d794ecfb6be2fe819a1ac71d92e7f67
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-10-09 06:44:48 -0300

    service to chamado-update implemented

--- src/app/components/chamado/chamado-update/chamado-update.component.ts
+++ src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Chamado } from 'src/app/models/chamado';
 import { Cliente } from 'src/app/models/cliente';
@@ -42,22 +42,33 @@ export class ChamadoUpdateComponent implements OnInit {
               private clienteService: ClienteService, 
               private tecnicoService: TecnicoService,
               private toastr: ToastrService, 
-              private router: Router) { }
+              private router: Router, 
+              private activateRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.chamado.id = this.activateRoute.snapshot.paramMap.get('id')
+    this.findById(); 
     this.findAllClientes();
     this.findAllTecnicos();
   }
 
-  create(): void{
-    this.chamadoService.create(this.chamado).subscribe(response => {
-      this.toastr.success('Chamado criado com sucesso', 'Novo Chamado'); 
+  findById(): void{
+      this.chamadoService.findById(this.chamado.id).subscribe(response => {
+        this.chamado = response; 
+      }, ex => {
+        this.toastr.error(ex.error.error); 
+      })
+  }
+
+
+  update(): void{
+    this.chamadoService.update(this.chamado).subscribe(response => {
+      this.toastr.success('Chamado atualizado com sucesso', 'Atualizar Chamado'); 
       this.router.navigate(['chamados'])
     }, ex => {
       this.toastr.error(ex.error.error);
     })
   }
-  
 
   findAllClientes(): void{
     this.clienteService.findAll().subscribe(response => {
@@ -71,7 +82,25 @@ export class ChamadoUpdateComponent implements OnInit {
     })
   }
 
+  retornaStatus(status: any): string {
+    if(status == '0'){
+      return 'ABERTO';
+    }else if(status == '1'){
+      return 'EM ANDAMENTO';
+    }else {
+      return 'ENCERRADO';
+    }
+  }
 
+  retornaPrioridade(prioridade: any): string {
+    if(prioridade == '0'){
+      return 'BAIXA';
+    }else if(prioridade == '1'){
+      return 'MÉDIA';
+    }else {
+      return 'ALTA';
+    }
+  }
 
   validaCampos(): boolean {
     return this.prioridade.valid && this.status.valid && this.titulo.valid && 
